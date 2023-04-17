7772a26b2d22e21f53d0267d99ab2549d9f31bbb
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2023-04-18 02:20:26 +0300

    models and migrations are finished

---
+++ laravel/database/migrations/2023_04_17_225825_create_projects_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->string('address', 255);
+            $table->longText('content')->nullable();
+            $table->string('slug', 255);
+
+            $table->unsignedBigInteger('sub-category_id')->nullable();
+            $table->foreign('sub-category_id')
+                ->references('id')
+                ->on("sub_categories")
+                ->onDelete('set null')
+                ->onUpdate('set null');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
